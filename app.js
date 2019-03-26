4c91ba9f8bd3ef9b0950ccca1537e47ed20c46b6
Esten <estengrove99@gmail.com>
2019-03-26 08:10:15 -0700

    Updated styling for Banner Sections, Change to better Logo in the Nav. FIXED.

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 /******************************************
- *  Author : Steven G.   
+ *  Author : Steven G.
  *  Created On : Wed Mar 20 2019
  *  File : app.js
  *******************************************/
 // Toggle Menu Icon
-const hamburgerIcon = document.querySelector('#hamburger-icon');
-console.log(hamburgerIcon);
+const hamburgerIcon = document.querySelector("#hamburger-icon");
 
 // Mobile Menu Items
-const mobileLinksWrapper = document.querySelector('.mobileLinks-wrapper');
+const mobileLinksWrapper = document.querySelector(".mobileLinks-wrapper");
 const closeIcon = "/img/sprite.svg#icon-cross";
-const openIcon = '/img/sprite.svg#icon-menu';
+const openIcon = "/img/sprite.svg#icon-menu";
 const iconEl = hamburgerIcon.children[0];
 
 // Function to Show/Hide Mobile Menu Items
-const showMenu = (e) => {
-    mobileLinksWrapper.classList.toggle('showMenu');
-    if (mobileLinksWrapper.classList.contains('showMenu')) {
-        iconEl.setAttribute('xlink:href', closeIcon);
-    } else {
-        iconEl.setAttribute('xlink:href', openIcon)
-    }
-}
-hamburgerIcon.addEventListener('click', showMenu);
-
-console.log(iconEl)
+const showMenu = e => {
+  mobileLinksWrapper.classList.toggle("showMenu");
+  if (mobileLinksWrapper.classList.contains("showMenu")) {
+    iconEl.setAttribute("xlink:href", closeIcon);
+  } else {
+    iconEl.setAttribute("xlink:href", openIcon);
+  }
+};
+hamburgerIcon.addEventListener("click", showMenu);
