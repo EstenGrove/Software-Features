f82a4ca7fa8bebacbfc5b19892d71f887e32dce3
Esten <estengrove99@gmail.com>
2019-03-27 21:46:31 -0700

    Added scrollToTop function and button. Tested functionality. COMPLETED

--- app.js
+++ app.js
@@ -22,3 +22,20 @@ const showMenu = e => {
   }
 };
 hamburgerIcon.addEventListener("click", showMenu);
+
+// Scroll to Top button
+const toTopBtn = document.getElementById("toTop");
+
+// If user scrolls 200px from top show button, else hide it
+window.onscroll = () => {
+  const scrollPos =
+    window.scrollY || window.pageYOffset || document.body.scrollTop;
+  if (scrollPos > 200) {
+    toTopBtn.style.display = "block";
+  } else {
+    toTopBtn.style.display = "none";
+  }
+};
+
+// Scroll to top when clicked
+toTopBtn.addEventListener("click", e => window.scrollTo(0, 0));
